5963151249bc87747f5ef4c9a41216225b0f4f94
Lanyun Hou <a72@luotianyi.pro>
2019-02-03 10:31:01 +0800

    Update RapChinese.py

--- python_test/ScoreDraft/RapChinese.py
+++ python_test/ScoreDraft/RapChinese.py
@@ -1,4 +1,4 @@
-baseFreq=1.0
+baseFreq=1.5
 
 def SetRapBaseFreq(freq):
 	global baseFreq   
@@ -6,12 +6,13 @@ def SetRapBaseFreq(freq):
 
 def CRap(lyric, tone, duration=48):
 	if tone <= 1:
-		return (lyric, duration, baseFreq, baseFreq)
+		return (lyric, duration, baseFreq*0.9, baseFreq*0.9)
 	elif tone == 2:
-		return (lyric, duration, baseFreq*0.7, baseFreq)
+		return (lyric, duration, baseFreq*0.7, baseFreq*0.85)
 	elif tone == 3:
 		return (lyric, duration, baseFreq*0.5, baseFreq*0.75)
 	elif tone == 4:
 		return (lyric, duration, baseFreq, baseFreq*0.5)
 	else:
 		return (lyric, duration, baseFreq*0.75, baseFreq*0.55)
+
